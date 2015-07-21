ae1fb25721d81f8bb911a0f9731481ad0fc8437b
Denis <denisfedak84@gmail.com>
2015-07-21 19:46:02 +0300

    Add Line and roundImageView (icon weather) in activity_main_rev01

--- app/src/main/java/net/validcat/fishing/ui/RoundedImageView.java
+++ app/src/main/java/net/validcat/fishing/ui/RoundedImageView.java
@@ -20,11 +20,10 @@ public class RoundedImageView extends ImageView {
     public RoundedImageView(Context ctx, AttributeSet attrs) {
         super(ctx, attrs);
     }
+
     @Override
     protected void onDraw(Canvas canvas) {
-
         Drawable drawable = getDrawable();
-
         if (drawable == null) {
             return;
         }
@@ -34,12 +33,9 @@ public class RoundedImageView extends ImageView {
         }
         Bitmap b = ((BitmapDrawable) drawable).getBitmap();
         Bitmap bitmap = b.copy(Bitmap.Config.ARGB_8888, true);
-
         int w = getWidth(), h = getHeight();
-
         Bitmap roundBitmap = getRoundedCroppedBitmap(bitmap, w);
         canvas.drawBitmap(roundBitmap, 0, 0, null);
-
     }
 
     public static Bitmap getRoundedCroppedBitmap(Bitmap bitmap, int radius) {

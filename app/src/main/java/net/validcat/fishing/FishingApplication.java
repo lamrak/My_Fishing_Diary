800ce97ef91a27fd0592f3ebc78385391fbfec59
pphilipp <philippdevel@gmail.com>
2017-03-26 16:29:20 +0300

    Update: eneble Firebase offline.

--- app/src/main/java/net/validcat/fishing/FishingApplication.java
+++ app/src/main/java/net/validcat/fishing/FishingApplication.java
@@ -4,11 +4,19 @@ import android.content.Context;
 import android.support.multidex.MultiDex;
 import android.support.multidex.MultiDexApplication;
 
+import com.google.firebase.database.FirebaseDatabase;
+
 public class FishingApplication extends MultiDexApplication {
 
     @Override
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
         MultiDex.install(this);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
     }
 }

f74073b7f986a03b8fafca0acbb95cbad0895bc9
pphilipp <philippdevel@gmail.com>
2017-04-21 12:05:49 +0300

    Update: Setup Fabrick, update version DB and app in manifest.

--- app/src/main/java/net/validcat/fishing/FishingApplication.java
+++ app/src/main/java/net/validcat/fishing/FishingApplication.java
@@ -1,13 +1,18 @@
 package net.validcat.fishing;
 
 import android.app.Application;
+
 import com.google.firebase.database.FirebaseDatabase;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class FishingApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
+
         FirebaseDatabase.getInstance().setPersistenceEnabled(true);
     }
 }
